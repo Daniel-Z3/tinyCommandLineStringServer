96aa99073ae491fb11226d291f84cf32f2d3626d
Daniel-Z3 <dzdanielz29@gmail.com>
2026-09-17 11:13:30 -0600

    Added more detailed status messages on testCLient and changed error return to 1

--- tests/testClient.c
+++ tests/testClient.c
@@ -3,8 +3,50 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main(){
-  
+int main(void){
+  void* buf;//Char buf to be cast
+  char* SERVER_PORT="19008";
+  struct addrinfo hints,*p,*res;
+  int status;
+  int sockfd;
+  memset(&hints,0,sizeof hints);
+
+
+  hints.ai_family=AF_INET;
+  hints.ai_socktype=SOCK_STREAM;
+
+  status=getaddrinfo("localhost",SERVER_PORT,&hints,&res);
+  if(status!=0){
+    printf("getaddrinfo Error: %s\n",gai_strerror(status));
+    return 1;
+  } 
+  sockfd=socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+  if(sockfd==-1){
+    printf("socket Error:\n");
+    return 1;
+  }
+  int constatus=connect(sockfd, res->ai_addr,res->ai_addrlen);
+  if(constatus==-1){
+    printf("connect Error: %s\n",strerror(errno));
+    return 1;
+  }
+  char svrIPstr[INET_ADDRSTRLEN];
+  inet_ntop(res->ai_family,&(((struct sockaddr_in *)res->ai_addr)->sin_addr),svrIPstr, sizeof svrIPstr);
+  printf("Connected to server on %s\n", svrIPstr);
+   
+  int recstatus=recv(sockfd,buf,7,0);
+  if(recstatus==-1){
+    printf("recv Error: %s\n", strerror(errno));
+    return 1;
+  }
+  printf("Recieved message: %s\n", buf);
+ 
+  close(sockfd);
+  freeaddrinfo(res);
   return 0;
 }
